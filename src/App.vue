685303c4c096a78735577a646e7c213fcbea3a51
lqh <15017662461@163.com>
2020-11-18 15:02:18 +0800

    配置vue3的router

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,28 @@
 <template>
   <div class="container">
     <globale-header :user="currentUser"></globale-header>
-    <!-- <column-list :list="testData"></column-list> -->
-    <validate-form @form-submit="onFormSubmit">
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
-        <validate-input ref="inputRef" v-model="emailVal" :rules="emailRules" type="text" placeholder="请输入邮箱地址"></validate-input>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">密码</label>
-        <validate-input v-model="passVal" :rules="passRules" type="password" placeholder="请输入密码"></validate-input>
-      </div>
-      <template #submit>
-        <span class="btn btn-danger">Submit</span>
-      </template>
-    </validate-form>
+    <router-view></router-view>
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">@ 2020 者也专栏</li>
+          <li class="list-inline-item">课程</li>
+          <li class="list-inline-item">文档</li>
+          <li class="list-inline-item">练习</li>
+          <li class="list-inline-item">更多</li>
+        </ul>
+      </small>
+    </footer>
+    
   </div>
 </template>
 
 <script lang="ts">
 import {
   defineComponent,
-  onMounted,
-  onUpdated,
-  reactive,
-  ref,
-  watch,
 } from "vue";
-import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
 import GlobaleHeader, { UserProps } from "./components/GlobalHeader.vue";
-import ValidateInput, { RulesProp } from "./components/ValidateInput.vue";
-import ValidateForm from './components/ValidateForm.vue'
-import { emitter } from './components/ValidateForm.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: "test1的专栏",
-    description: "这是test1的专栏，有一段非常有益的见解，来更新吧",
-    avatar: "123",
-  },
-  {
-    id: 1,
-    title: "test1的专栏",
-    description: "这是test1的专栏，有一段非常有益的见解，来更新吧",
-    avatar: "123",
-  },
-  {
-    id: 1,
-    title: "test1的专栏",
-    description: "这是test1的专栏，有一段非常有益的见解，来更新吧",
-    avatar: "123",
-  },
-  {
-    id: 1,
-    title: "test1的专栏",
-    description: "这是test1的专栏，有一段非常有益的见解，来更新吧",
-    avatar: "123",
-  },
-  {
-    id: 1,
-    title: "test1的专栏",
-    description: "这是test1的专栏，有一段非常有益的见解，来更新吧",
-    // avatar:'123'
-  },
-  {
-    id: 1,
-    title: "test1的专栏",
-    description: "这是test1的专栏，有一段非常有益的见解，来更新吧",
-    avatar: "123",
-  },
-];
 
 const currentUser: UserProps = {
   isLogin: true,
@@ -81,36 +33,12 @@ const currentUser: UserProps = {
 export default defineComponent({
   name: "App",
   components: {
-    // ColumnList,
     GlobaleHeader,
-    ValidateInput,
-    ValidateForm,
   },
   setup() {
-    const emailVal = ref('')
-    const passVal = ref('')
-    const inputRef = ref<any>(null)
-    const emailRules: RulesProp = [
-      { type: "required", message: "电子邮箱不能为空" },
-      { type: "email", message: "请输入正确的邮箱地址" },
-    ];
-    const passRules: RulesProp = [
-      { type: "required", message: "密码不能为空" },
-    ];
-    const onFormSubmit = (result: boolean) => {
-      console.log(result)
-      emitter.emit('clear-value')
-    }
 
     return {
-      testData,
       currentUser,
-      emailRules, 
-      passRules,
-      emailVal,
-      passVal,
-      onFormSubmit,
-      inputRef
     };
   },
 });
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,35 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import ColumnDetail from './views/ColumnDetail.vue'
 import App from './App.vue'
+
+
+const routerHistory = createWebHistory()
+const router = createRouter({
+  history:routerHistory,
+  routes:[
+    {
+      path: '/',
+      name: 'home',
+      component:Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component:Login
+    },
+    {
+      path: '/column/:id',
+      name: 'column',
+      component:ColumnDetail,
+      meta:{x:1}
+    }
+  ]
+})
+
 import './assets/css/reset.css'
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
