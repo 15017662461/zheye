eba53356be61da6d3ad01d3d146b8a5104db5593
lqh <15017662461@163.com>
2021-06-03 11:29:02 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
         ws: true,
         pathRewrite: {
           '^/api': ''      //这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替
-          // 比如我要调用'http://localhost:8024/management/user/add'，直接写‘/api/user/add’即可
+          // 比如我要调用'http://api.vikingship.xyz/api/user/add'，直接写‘/api/user/add’即可
         }
       }
     }
