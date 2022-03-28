2a8c7dfedfa296fa31607aa124ce38ca0d2dfc19
15017662461 <15017662461@163.com>
2022-03-28 12:21:41 +0800

    fix: 修复了在专栏详情页跳转至我的详情页时页面不变化的bug

--- src/views/ColumnDetail.vue
+++ src/views/ColumnDetail.vue
@@ -2,11 +2,15 @@
   <div class="w-690 column-detail-page">
     <div class="column-info row mb-4 border-bottom pb-4 align-items-center">
       <div class="col-3 text-center">
-        <img :src="column && column.avatar && column.avatar.fitUrl" :alt="column && column.description" class="rounded-circle border w-100">
+        <img
+          :src="column && column.avatar && column.avatar.fitUrl"
+          :alt="column && column.description"
+          class="rounded-circle border w-100"
+        />
       </div>
       <div class="col-9">
-        <h4>{{column && column.title}}</h4>
-        <p class="text-muted">{{column && column.description}}</p>
+        <h4>{{ column && column.title }}</h4>
+        <p class="text-muted">{{ column && column.description }}</p>
       </div>
     </div>
     <post-list :list="list"></post-list>
@@ -14,44 +18,58 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, onMounted } from "vue"
-import { useRoute } from 'vue-router'
-import { useStore } from 'vuex'
-import { GlobalDataProps, ColumnProps } from '../store'
-import PostList from '../components/PostList.vue'
-import { addColumnAvatar } from "../helper"
+import { defineComponent, computed, onMounted, watch, ref } from "vue";
+import { useRoute } from "vue-router";
+import { useStore } from "vuex";
+import { GlobalDataProps, ColumnProps } from "../store";
+import PostList from "../components/PostList.vue";
+import { addColumnAvatar } from "../helper";
 export default defineComponent({
-  name:'ColumnDetail',
-  components:{
-    PostList
+  name: "ColumnDetail",
+  components: {
+    PostList,
   },
-  setup(){
-    const route = useRoute()
-    const store = useStore<GlobalDataProps>()
-    const currenId = route.params.id
-    const column = computed(() => {
-      const selectColumn = store.getters.getColumnById(currenId) as ColumnProps | undefined
-      if (selectColumn) {
-        addColumnAvatar(selectColumn, 100, 100)
-      }
-      return selectColumn
-    })
-    const list = computed(() => store.getters.getPostsByCid(currenId))
-    onMounted(() => {
-      store.dispatch('fetchColumn',currenId)
-      store.dispatch('fetchPosts',currenId)
-    })
-    return{
-      column,
-      list
-    }
-  }
+  setup() {
+    const route = useRoute();
+    const store = useStore<GlobalDataProps>();
+    const currenId = ref(route.params.id);
 
-})
+    const fetchData = () => {
+      store.dispatch("fetchColumn", currenId.value);
+      store.dispatch("fetchPosts", currenId.value);
+    };
+
+    watch(
+      () => route.params.id,
+      () => {
+        currenId.value = route.params.id;
+        fetchData();
+      }
+    );
+
+    const column = computed(() => {
+      const selectColumn = store.getters.getColumnById(currenId.value) as
+        | ColumnProps
+        | undefined;
+      if (selectColumn) {
+        addColumnAvatar(selectColumn, 100, 100);
+      }
+      return selectColumn;
+    });
+    const list = computed(() => store.getters.getPostsByCid(currenId.value));
+    onMounted(() => {
+      fetchData();
+    });
+    return {
+      column,
+      list,
+    };
+  },
+});
 </script>
 <style scoped>
-  .w-690{
-    width: 690px;
-    margin: 0 auto;
-  }
+.w-690 {
+  width: 690px;
+  margin: 0 auto;
+}
 </style>
