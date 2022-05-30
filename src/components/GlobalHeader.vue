b0e5c0469c401e17170f3af601a03978a51c7573
15017662461 <15017662461@163.com>
2022-05-30 18:26:23 +0800

    docs: test

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
     <div class="w-75 mx-auto navbar">
-      <a href="#" class="navbar-brand" @click="clickToHome">者也专栏</a>
+      <a href="#" class="navbar-brand" @click="clickToHome">毕设专栏</a>
       <ul v-if="!user.isLogin" class="list-inline mb-0">
         <li class="list-inline-item">
           <a href="#" class="btn btn-outline-light my-2" @click="clickToLogin">登录</a>
