ac56b8b1af70a6d652d2107b87513289dcf56860
15017662461 <15017662461@163.com>
2022-05-30 15:08:21 +0800

    docs: test

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <footer class="text-center py-4 text-secondary bg-light mt-6">
       <small>
         <ul class="list-inline mb-0">
-          <li class="list-inline-item">@ 2022 毕设专栏</li>
+          <li class="list-inline-item">@ 2022 者也专栏</li>
           <li class="list-inline-item">课程</li>
           <li class="list-inline-item">文档</li>
           <li class="list-inline-item">练习</li>
